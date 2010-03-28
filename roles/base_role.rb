9ee950a58d9198c6dd0fe567bc239e1080068188
Philip (flip) Kromer <flip@infochimps.org>
2010-03-28 18:47:02 -0500

    Simple base role for all nodes

---
+++ roles/base_role.rb
@@ -0,0 +1,32 @@
+require File.dirname(__FILE__)+'/../settings'
+
+# Install with
+#   knife role from file roles/base_role.rb
+
+name 'base_role'
+description 'top level attributes, applies to all nodes'
+
+run_list *%w[
+  users::env
+  users::homes
+  sudo
+  emacs
+  git
+  base_recipe
+  ]
+
+# Attributes applied if the node doesn't have it set already.
+default_attributes(
+  "base_role" => {
+    'home_base_dir'         => '/home',
+    'mnt_point'             => '/mnt',
+    'bin_dir'               => '/usr/local/bin',
+    'upstart_event_dir'     => '/etc/init',
+    'target_user'           => 'ubuntu',
+    'aws_access_key'        => Settings[:access_key],
+    'aws_secret_access_key' => Settings[:secret_access_key],
+    'availability_zone'     => Settings[:availability_zones].first,
+  })
+
+# # Attributes applied no matter what the node has set already.
+# override_attributes()
