f0500a7a4294fea2e107c3244c236b3cf1f7849b
Philip (flip) Kromer <flip@infochimps.org>
2010-03-31 04:31:56 -0500

    Rectification of names

---
+++ cookbooks/hadoop_cluster/metadata.rb
@@ -0,0 +1,11 @@
+maintainer        "Infochimps.org"
+maintainer_email  "help@infochimps.org"
+license           "Apache 2.0"
+description       "Installs hadoop and sets up a high-performance cluster. Inspired by Tom White / Cloudera's hadoop-ec2 command line utilities"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version           "0.8"
+depends           "java"
+
+%w{ debian ubuntu }.each do |os|
+  supports os
+end
