23651f4ae6ac37d30a3fa47669a94248289c83d7
Philip (flip) Kromer <flip@infochimps.org>
2010-04-06 03:30:06 -0500

    Cleaning out non-hadoop_cluster specific stuff

--- site-cookbooks/big_package/recipes/emacs_modes.rb
+++
@@ -1,9 +0,0 @@
-
-%w[
-  erlang-mode python-mode ruby-elisp ruby1.8-elisp php-mode org-mode nxml-mode
-  mmm-mode css-mode gnuplot-mode html-helper-mode lua-mode
-].each do |pkg|
-  package pkg do
-    action :upgrade
-  end
-end
