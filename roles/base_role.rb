741a1f8bc0388471161431d316b0438fee525d8c
Philip (flip) Kromer <flip@infochimps.org>
2010-04-06 02:26:56 -0500

    oh right added aws keys back to hadoop role

--- roles/base_role.rb
+++ roles/base_role.rb
@@ -1,9 +1,5 @@
-# Install with
-#   knife role from file roles/base_role.rb
-
 name        'base_role'
 description 'top level attributes, applies to all nodes'
-require File.dirname(__FILE__)+'/../settings'
 
 run_list *%w[
   base
--- roles/hadoop.rb
+++ roles/hadoop.rb
@@ -1,10 +1,7 @@
-require File.dirname(__FILE__)+'/../settings'
-
-# Install with
-#   knife role from file roles/base_role.rb
-
-name 'hadoop'
+# Install with `knife role from file roles/hadoop.rb`
+name        'hadoop'
 description 'applies to all nodes in the hadoop cluster'
+require File.dirname(__FILE__)+'/../settings'
 
 run_list *%w[
   hadoop_cluster
@@ -21,5 +18,12 @@ default_attributes({
     :hadoop => {
       :hadoop_handle => 'hadoop-0.20',
       :cdh_version   => 'cdh3',
-    }
+    },
+    # Needed if you want to access S3 files via s3n:// and s3:// urls
+    :aws => {
+      :aws_access_key        => Settings[:access_key],
+      :aws_secret_access_key => Settings[:secret_access_key],
+      :aws_region            => Settings[:aws_region],
+      :availability_zone     => Settings[:availability_zones].first,
+    },
   })
