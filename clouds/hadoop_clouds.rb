f19987457e8c5d0d6847851e42b611fdb79c11ab
Philip (flip) Kromer <flip@infochimps.org>
2010-04-07 11:38:40 -0500

    trying to burn an ami and not screw it up

--- clouds/hadoop_clouds.rb
+++ clouds/hadoop_clouds.rb
@@ -33,7 +33,7 @@ pool POOL_NAME do
   cloud :slave do
     using :ec2
     settings = settings_for_node(POOL_NAME, :slave)
-    instances            1..1
+    instances            2..2
     attaches_ebs_volumes settings
     is_nfs_client        settings
     is_generic_node      settings
