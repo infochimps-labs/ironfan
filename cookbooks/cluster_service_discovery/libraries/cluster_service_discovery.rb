fcd94c272c775e1b25d01cf987f66839d6030e46
Philip (flip) Kromer <flip@infochimps.org>
2010-05-02 19:52:40 -0500

    Added service discovery mechanism

--- cookbooks/cluster_service_discovery/libraries/cluster_service_discovery.rb
+++ cookbooks/cluster_service_discovery/libraries/cluster_service_discovery.rb
@@ -4,62 +4,74 @@
 # Since there's no good way to do this yet, let's at least put an abstraction in
 # place.
 #
-# This module lets all the various moving parts register themselves for a role
+# This module lets any node
+# * register for a service. A timestamp records the last registry.
+# * discover all chef nodes that have registered for the given service.
+# * discover the most recent chef node for that service.
+# * get the 'public_ip' for a service -- the address that nodes in the larger
+#   world should use
+# * get the 'public_ip' for a service -- the address that nodes on the local
+#   subnet / private cloud should use
 #
-# Currently, it maintains a databag called 'servers_info', with items named for
-# each cluster_role
+# Implementation
+#
+# Nodes register a service by setting the +[:provides_service][service_name]+
+# attribute. This attribute is a hash containing at 'timestamp' (the time of
+# registry), but the service can pass in an arbitrary hash of values to merge
+# in.
 #
 module ClusterServiceDiscovery
 
-  # Look in the 'servers_info' databag for the 
-  def address_for_role role
-    host_for_role(role)['private_ip'] rescue nil
+  # Find all nodes that have indicated they provide the given service,
+  # in descending order of when they registered.
+  def nodes_for_service service_name
+    search(:node, "provides_service:#{service_name}"
+      ).sort_by{|server| server[:provides_service][service_name]['timestamp'] } rescue []
   end
 
-  def host_for_role role
-    data_bag_item('servers_info', "#{node[:cluster_name]}-#{role}")
+  def node_for_service service_name
+    nodes_for_service.first
   end
 
-  def register_for_role cluster_role
-    node.set[:provides_service][cluster_role] = {
-      :timestamp => timestamp,
-      :private_ip => my_private_ip, :public_ip => my_public_ip, :default_ip => my_default_ip, :fqdn => my_fqdn  }
+  # Set the
+  def register_for_service service_name, service_info={}
+    node.set[:provides_service][service_name] = {
+      :timestamp  => ClusterServiceDiscovery.timestamp,
+    }.merge(service_info)
+    node.save
   end
 
-  def search_for_node node_name
-    search(:node, node_name) do |server|
-      node[:cloud_private_ips]
-    end
-  end
-
-  def self.my_default_ip()        node[:ipaddress]                            ; end
-  def self.my_fqdn()              node[:fqdn]                                 ; end
-  def self.my_availability_zone() node[:ec2][:availability_zone]              ; end
-  def self.timestamp()            Time.now.utc.strftime("%Y%m%d%H%M%SZ")      ; end
-
-  # Return the private ip address for this node: the ip address a machine on the
-  # local subnet would want to reach.
-  def self.my_private_ip
-    node[:cloud][:private_ips].first rescue default_ip
+  # Return the private ip address for this node: the ip address a machine in the
+  # local subnet / private cloud would want to reach.
+  def service_private_ip service_name
+    node = node_for_service(service_name) or return
+    node[:cloud][:private_ips].first rescue node[:ipaddress]
   end
 
   # Return the public ip address for this node: the ip address a machine in the
   # larger world would want to reach.
-  def self.my_public_ip
-    node[:cloud][:public_ips].first rescue default_ip
+  def service_public_ip service_name
+    node = node_for_service(service_name) or return
+    node[:cloud][:public_ips].first rescue node[:ipaddress]
   end
 
-  # a = search(:node, "provides_service:nfs_server").sort_by{|server| server[:provides_service][:nfs_server][:timestamp] } ; gm=a.first ; gm.name
-  # gm[:provides_service][:nfs_server]
-  # a = search(:node, "role:hadoop_master") ; a.first.name
-  # a.select{|node| node.name =~ /gibbon-master/ }.sort_by(&:name).first; gm.name
-  # gm[:cloud].to_hash
-  # a = search :node, "name:gibbon*"  ; a.length
-  # gm = a.reject{|n| n.run_list.grep(/hadoop_master/).blank? }.first ; gm.name
-  
-end
+  # A compact timestamp, to record when services are registered
+  def self.timestamp
+    Time.now.utc.strftime("%Y%m%d%H%M%SZ")
+  end
 
+end
 class Chef::Recipe              ; include ClusterServiceDiscovery ; end
 class Chef::Resource::Directory ; include ClusterServiceDiscovery ; end
 class Chef::Resource            ; include ClusterServiceDiscovery ; end
+
+
+
+# a = search(:node, "provides_service:nfs_server").sort_by{|server| server[:provides_service][:nfs_server][:timestamp] } ; gm=a.first ; gm.name
+# gm[:provides_service][:nfs_server]
+# a = search(:node, "role:hadoop_master") ; a.first.name
+# a.select{|node| node.name =~ /gibbon-master/ }.sort_by(&:name).first; gm.name
+# gm[:cloud].to_hash
+# a = search :node, "name:gibbon*"  ; a.length
+# gm = a.reject{|n| n.run_list.grep(/hadoop_master/).blank? }.first ; gm.name
 
