5b91e714eb480df223193265ab037ba4f18fd50b
Philip (flip) Kromer <flip@infochimps.org>
2010-05-05 15:50:18 -0500

    Nuked obsolete instructions re: databags

--- config/knife.rb
+++ config/knife.rb
@@ -7,7 +7,7 @@ validation_key           ENV['HOME']+'/.chef/keypairs/chef-validator.pem'
 chef_server_url          'http://chef.infinitemonkeys.info:4000'
 cache_type               'BasicFile'
 PATH_TO_COOKBOOK_REPOS = File.join(ENV['HOME'], 'ics/sysadmin')
-cookbook_path            [ 'hadoop_cluster_chef/cookbooks', 'hadoop_cluster_chef/site-cookbooks', 'infochimps_chef/cookbooks', 'infochimps_chef/site-cookbooks',
+cookbook_path            [ 'hadoop_cluster_chef/cookbooks',
   ].map{|path| File.join(PATH_TO_COOKBOOK_REPOS, path) }
 cache_options( :path => File.join(ENV['HOME'], '.chef/chef_checksums') )
 
