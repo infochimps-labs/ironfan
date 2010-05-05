06cb7f0c6bb846d01531a9aa7591d1d1e7684775
Philip (flip) Kromer <flip@infochimps.org>
2010-05-05 13:44:26 -0500

    updated to new poolparty.yaml; also some notes taken debugging with @cmetcalfe

--- config/knife.rb
+++ config/knife.rb
@@ -7,6 +7,7 @@ validation_key           ENV['HOME']+'/.chef/keypairs/chef-validator.pem'
 chef_server_url          'http://chef.infinitemonkeys.info:4000'
 cache_type               'BasicFile'
 PATH_TO_COOKBOOK_REPOS = File.join(ENV['HOME'], 'ics/sysadmin')
+cookbook_path            [ 'hadoop_cluster_chef/cookbooks', 'hadoop_cluster_chef/site-cookbooks', 'infochimps_chef/cookbooks', 'infochimps_chef/site-cookbooks',
+  ].map{|path| File.join(PATH_TO_COOKBOOK_REPOS, path) }
 cache_options( :path => File.join(ENV['HOME'], '.chef/chef_checksums') )
-cookbook_path            [ 'hadoop_cluster_chef/cookbooks', 'your_custom_chef/cookbooks', 'your_custom_chef/site-cookbooks',
-  ].map{|path| File.expand_path(File.join(PATH_TO_COOKBOOK_REPOS, path)) }
+
