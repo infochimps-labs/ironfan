41b8877078dac2d01eec8fb007703db503b87a6f
Dhruv Bansal <dhruv@ph.utexas.edu>
2010-05-25 00:05:58 -0500

    Added azkaban role, not tested.

---
+++ roles/azkaban.rb
@@ -0,0 +1,13 @@
+name        'azkaban'
+description 'Runs azkaban'
+
+# FIXME what are these?
+run_list *%w[
+  java
+  runit
+  azkaban::install_from_release
+  ]
+
+# Attributes applied if the node doesn't have it set already.
+default_attributes({
+  })
