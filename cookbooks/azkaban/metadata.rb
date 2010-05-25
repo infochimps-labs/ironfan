72b0d6adb53c88dec7f7763672e2975955fb3912
Dhruv Bansal <dhruv@ph.utexas.edu>
2010-05-25 00:04:58 -0500

    Added Azkaban cookbook.  Not tested.

---
+++ cookbooks/azkaban/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Infochimps Org"
+maintainer_email "coders@infochimps.org"
+license          "Apache 2.0"
+description      "Installs/Configures azkaban"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
