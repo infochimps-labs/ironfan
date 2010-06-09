b2c71eb12f91a7c44d41199a08a1b93211596227
Jacob <jacob.a.perkins@gmail.com>
2010-06-09 15:45:51 -0500

    zaius master is cassandra node for now as well

--- clouds/zaius_clouds.rb
+++ clouds/zaius_clouds.rb
@@ -50,6 +50,7 @@ pool POOL_NAME do
     is_chef_client              settings
     #
     is_hadoop_node              settings
+    is_cassandra_node           settings
     has_role   settings, "hadoop_master"
     has_role   settings, "hadoop_worker"
     has_recipe settings, 'hadoop_cluster::std_hdfs_dirs'
