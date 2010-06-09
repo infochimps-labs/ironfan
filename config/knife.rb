ec6c728ad5a85685466b6c6c4c4a72f9096d2f54
Philip (flip) Kromer <flip@infochimps.org>
2010-06-09 18:40:56 -0500

    updated config files to match current reality

--- config/knife.rb
+++ config/knife.rb
@@ -1,10 +1,10 @@
+chef_server_url          'http://chef.yourdomain.com:4000'
 log_level                :info
 log_location             STDERR
 node_name                'knife_user'
 validation_client_name   'chef-validator'
 client_key               ENV['HOME']+'/.chef/keypairs/knife_user.pem'
 validation_key           ENV['HOME']+'/.chef/keypairs/chef-validator.pem'
-chef_server_url          'http://chef.infochimps.com:4000'
 cache_type               'BasicFile'
 CHEF_COOKBOOKS_ROOT = File.expand_path(ENV['CHEF_COOKBOOKS_ROOT'] || '~/ics/sysadmin')
 cookbook_path            [ 'cluster_chef/cookbooks',
