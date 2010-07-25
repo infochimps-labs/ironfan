d1247a8b9ecd948aa0771012a97cdbcec02a4532
Philip (flip) Kromer <flip@infochimps.org>
2010-07-24 21:40:42 -0500

    updated java to chef v0.9-compatible cookbook in http://github.com/opscode/cookbooks repo

--- cookbooks/java/metadata.rb
+++ cookbooks/java/metadata.rb
@@ -1,10 +1,12 @@
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
-description       "Installs java"
+description       "Installs java via openjdk."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.9"
+version           "0.10.2"
 
-%w{ debian ubuntu }.each do |os|
+recipe "java", "Installs openjdk to provide Java"
+
+%w{ debian ubuntu redhat centos fedora }.each do |os|
   supports os
 end
