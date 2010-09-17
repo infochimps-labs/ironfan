5bfdb962188d9ec7d5d7fad75547b10258a37b30
Jacob Perkins <jacob.a.perkins@gmail.com>
2010-09-17 18:16:35 -0500

    hdfs fuse now online; sweet.

--- cookbooks/hadoop_cluster/recipes/hdfs_fuse.rb
+++ cookbooks/hadoop_cluster/recipes/hdfs_fuse.rb
@@ -1,12 +1,38 @@
+class Chef::Recipe; include HadoopCluster ; end
+
 package "hadoop-0.20-fuse"
 include_recipe "runit"
 
 directory "/hdfs" do
-  owner    "root"
-  group    "root"
+  owner    "hadoop"
+  group    "hadoop"
   mode     "0755"
   action   :create
   recursive true
 end
 
-runit_service "hdfs_fuse"
+execute "add fuse module to kernel" do
+  command %Q{/sbin/modprobe fuse; true}
+end
+
+execute 'fix fuse configuration to allow hadoop' do
+  command %Q{sed -i -e 's|#user_allow_other|user_allow_other|' /etc/fuse.conf && chown hadoop:admin /etc/fuse.conf}
+  user 'root'
+  not_if "egrep '^user_allow_other' /etc/fuse.conf"
+end
+
+# this is clearly garbage, why isn't this a node[:os_arch] something?
+java_home_dir_mapping = {
+  'x86_64' => 'amd64',
+  'i686'   => 'i386'
+}
+
+template_parameters = {
+  :os_arch          => java_home_dir_mapping[ node[:kernel][:machine] ],
+  :namenode_address => namenode_address
+}
+Chef::Log.info template_parameters.inspect
+runit_service "hdfs_fuse" do
+  finish_script true
+  options(template_parameters)
+end
