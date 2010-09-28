0a518085ac6971168eca739ffbd7bf3d8f6645fb
Jacob Perkins <jacob.a.perkins@gmail.com>
2010-09-27 20:29:11 -0500

    some notes on installing R

---
+++ cookbooks/R/recipes/default.rb
@@ -0,0 +1,6 @@
+#this is not fun
+
+# sudo apt-get install r-base
+# sudo gem install rsruby -- --with-R-dir=/usr/share/R --with-R-lib=/usr/lib/R --with-R-include=/usr/share/R/include
+
+# Once R is installed you'll want to install some essential packages like VGAM and ggplot, ie. install.packages('VGAM')
