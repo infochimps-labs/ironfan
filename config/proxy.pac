a1efaa5b2a96937d02dc18ab9fe3c42407603896
Philip (flip) Kromer <flip@infochimps.org>
2010-10-28 19:23:26 -0500

    Updated the hadoop proxy .pac for a new naming scheme -- name like domU-21-31-93-0A-EA-56.compute-1.internal.  ---- Also, took apache2 out of the subversion cookbook; the mostly-harmless subversion cookbook is required for arcane reasons; in default it claims a dependency on apache2 but that's only if you want to have it serve files. We don't, so I've removed it as a mandatory depend

--- config/proxy.pac
+++ config/proxy.pac
@@ -3,7 +3,7 @@ function FindProxyForURL(url, host) {
   if ((shExpMatch(host, "*ec2*.amazonaws.com*")) ||
       (shExpMatch(host, "*ec2.internal*")) ||
       (shExpMatch(host, "*://10.*")) ||
-      (shExpMatch(host, "*compute.internal*")) ||
+      (shExpMatch(host, "*compute-*.internal*")) ||
       (shExpMatch(host, "*domu*.internal*"))) {
     return "SOCKS localhost:6666";
   }
--- cookbooks/subversion/metadata.rb
+++ cookbooks/subversion/metadata.rb
@@ -2,13 +2,14 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs subversion"
-version           "0.8.3"
+version           "0.8.4"
 
 %w{ redhat centos fedora ubuntu debian }.each do |os|
   supports os
 end
 
-depends "apache2"
+# depends "apache2" # argh. This is only needed for the server part. We don't
+                    # want that for the hadoop recipes, but maybe you do.
 
 recipe "subversion", "Includes the client recipe."
 recipe "subversion::client", "Subversion Client installs subversion and some extra svn libs"
