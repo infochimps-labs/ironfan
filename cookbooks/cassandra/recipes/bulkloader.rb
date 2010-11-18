4563d3782b90c0f1037c2ee3060e23d8a6b21df9
Jacob Perkins <jacob.a.perkins@gmail.com>
2010-11-18 10:01:34 -0600

    hacky recipe for bulkloader

---
+++ cookbooks/cassandra/recipes/bulkloader.rb
@@ -0,0 +1,21 @@
+# This stuff needs to be owned by hadoop
+
+[ "/var/lib/cassandra", "/var/log/cassandra",
+  node[:cassandra][:data_file_dirs],
+  node[:cassandra][:commitlog_dir],
+].flatten.each do |cassandra_dir|
+  directory cassandra_dir do
+    owner    "hadoop"
+    group    "root"
+    mode     "0755"
+    action   :create
+    recursive true
+  end
+end
+
+bash 'move updated json libs to hadoop lib' do
+  code <<EOF
+  rm /usr/lib/hadoop/lib/*jackson*.jar
+  cp /usr/local/share/cassandra/lib/*jackson*.jar /usr/lib/hadoop/lib/
+EOF
+end
