5af2980e7cdfdc1afdff66474735744278b3545e
Philip (flip) Kromer <flip@infochimps.org>
2011-01-21 00:54:44 -0600

    cleaned up config/ dir

--- clouds/zaius_clouds.rb
+++ clouds/zaius_clouds.rb
@@ -9,29 +9,6 @@ require File.dirname(__FILE__)+'/cloud_aspects'
 pool 'zaius' do
 
   #
-  # Combined chef server, nfs server and hadoop master. Run this if you're just
-  # starting out.
-  #
-  cloud :chefmaster do
-    using :ec2
-    settings = settings_for_node
-    instances                   1
-    is_generic_node             settings
-    is_nfs_server               settings
-    is_chef_server              settings
-    #
-    is_hadoop_node              settings
-    has_recipe                  settings, 'hadoop_cluster::format_namenode_once'
-    has_role                    settings, "hadoop_master"
-    has_role                    settings, "hadoop_worker"
-    has_recipe                  settings, 'hadoop_cluster::std_hdfs_dirs'
-    #
-    has_big_package             settings
-    has_role                    settings, "#{settings[:cluster_name]}_cluster"
-    user_data_is_bootstrap_script(settings, 'bootstrap_chef_server')
-  end
-
-  #
   # Hadoop master, to be used with a standalone chef server and (optional) nfs server.
   #
   cloud :master do
@@ -40,7 +17,7 @@ pool 'zaius' do
     instances                   1
     # Order matters here: specifically, attaches_ebs > nfs_client > most things
     is_generic_node             settings
-    is_nfs_client               settings
+    is_nfs_server               settings
     is_chef_client              settings
     #
     is_hadoop_node              settings
