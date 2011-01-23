3c615b68603f67c2d2e4a7274762564cd20565b1
Philip (flip) Kromer <flip@infochimps.org>
2011-01-22 21:23:23 -0600

    Made knife.rb work nicely with multiple organizations

--- config/knife.rb
+++ config/knife.rb
@@ -1,13 +1,43 @@
-chef_server_url          'http://chef.yourdomain.com:4000'
+current_dir   = File.dirname(__FILE__)
+organization  = ENV['CHEF_ORGANIZATION']      || 'YOUR_ORGANIZATION'
+username      = ENV['CHEF_USERNAME']          || 'YOUR_USERNAME'
+cookbook_root = ENV['PATH_TO_COOKBOOK_REPOS'] || '~/ics/sysadmin'
+
 log_level                :info
 log_location             STDERR
-node_name                'knife_user'
-validation_client_name   'chef-validator'
-client_key               ENV['HOME']+'/.chef/keypairs/knife_user.pem'
-validation_key           ENV['HOME']+'/.chef/keypairs/chef-validator.pem'
+node_name                username
+validation_client_name   "#{organization}-validator"
+client_key               "#{current_dir}/keypairs/#{username}-client_key.pem"
+validation_key           "#{current_dir}/keypairs/#{organization}-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/#{organization}"
 cache_type               'BasicFile'
-CHEF_COOKBOOKS_ROOT = File.expand_path(ENV['CHEF_COOKBOOKS_ROOT'] || '~/ics/sysadmin')
-cookbook_path            [ 'cluster_chef/cookbooks',
-  ].map{|path| File.join(CHEF_COOKBOOKS_ROOT, path) }
-cache_options( :path => File.expand_path('~/.chef/chef_checksums') )
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            [
+  'cluster_chef/cookbooks', 'cluster_chef/site-cookbooks',
+  ].map{|path| File.join(File.expand_path(cookbook_root), path) }
 
+#
+# Make sure to
+#
+# $ ssh-agent
+#
+# $ ssh-add ~/.ssh/ec2_ssh_key
+# SSH_AUTH_SOCK=/tmp/ssh-RHHBa22270/agent.22270; export SSH_AUTH_SOCK;
+# SSH_AGENT_PID=22271; export SSH_AGENT_PID;
+# echo Agent pid 22271;
+
+# EC2:
+knife[:aws_access_key_id]      = "AWS_KEY"
+knife[:aws_secret_access_key]  = "AWS_SECRET_ACCESS_KEY"
+
+# Rackspace:
+knife[:rackspace_api_key]      = "Your Rackspace API Key"
+knife[:rackspace_api_username] = "Your Rackspace API username"
+
+# Slicehost
+knife[:slicehost_password]     = "Your Slicehost API password"
+
+# # Terremark
+# knife[:terremark_password]   = "Your Terremark Password"
+# knife[:terremark_username]   = "Your Terremark Username"
+# knife[:terremark_service]    = "Your Terremark Service name"
