e350eb8d823dd08a51a1864852ebba16c698fec7
Philip (flip) Kromer <flip@infochimps.org>
2011-01-24 08:26:06 -0600

    put the readme as the readme

--- config/knife.rb
+++ config/knife.rb
@@ -1,7 +1,7 @@
 current_dir   = File.dirname(__FILE__)
 organization  = ENV['CHEF_ORGANIZATION']      || 'YOUR_ORGANIZATION'
 username      = ENV['CHEF_USERNAME']          || 'YOUR_USERNAME'
-cookbook_root = ENV['PATH_TO_COOKBOOK_REPOS'] || '~/ics/sysadmin'
+cookbook_root = ENV['PATH_TO_COOKBOOK_REPOS'] || File.expand_path('~/ics/sysadmin')
 
 log_level                :info
 log_location             STDERR
@@ -13,8 +13,9 @@ chef_server_url          "https://api.opscode.com/organizations/#{organization}"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            [
-  'cluster_chef/cookbooks', 'cluster_chef/site-cookbooks',
-  ].map{|path| File.join(File.expand_path(cookbook_root), path) }
+  "cluster_chef/cookbooks",         "cluster_chef/site-cookbooks",
+  "#{organization}_chef/cookbooks", "#{organization}_chef/site-cookbooks",
+  ].map{|path| File.join(cookbook_root, path) }
 
 #
 # Make sure to
@@ -26,19 +27,19 @@ cookbook_path            [
 # SSH_AGENT_PID=22271; export SSH_AGENT_PID;
 # echo Agent pid 22271;
 
-# EC2:
-knife[:aws_access_key_id]      = "AWS_KEY"
-knife[:aws_secret_access_key]  = "AWS_SECRET_ACCESS_KEY"
-knife[:aws_account_id]         = "AWS_ACCOUNT_ID"
-
-# Rackspace:
-knife[:rackspace_api_key]      = "Your Rackspace API Key"
-knife[:rackspace_api_username] = "Your Rackspace API username"
-
-# Slicehost
-knife[:slicehost_password]     = "Your Slicehost API password"
-
-# # Terremark
-# knife[:terremark_password]   = "Your Terremark Password"
-# knife[:terremark_username]   = "Your Terremark Username"
-# knife[:terremark_service]    = "Your Terremark Service name"
+case organization
+when "YOUR_PRODUCTION_ORG"
+  knife[:aws_access_key_id]      = "AWS_KEY"
+  knife[:aws_secret_access_key]  = "AWS_SECRET_ACCESS_KEY"
+  knife[:aws_account_id]         = "AWS_ACCOUNT_ID"
+  # knife[:rackspace_api_key]      = "Your Rackspace API Key"
+  # knife[:rackspace_api_username] = "Your Rackspace API username"
+  # knife[:slicehost_password]     = "Your Slicehost API password"
+  # knife[:terremark_password]   = "Your Terremark Password"
+  # knife[:terremark_username]   = "Your Terremark Username"
+  # knife[:terremark_service]    = "Your Terremark Service name"
+when "YOUR_DEVELOPMENT_ORG"
+  knife[:aws_access_key_id]      = "AWS_KEY"
+  knife[:aws_secret_access_key]  = "AWS_SECRET_ACCESS_KEY"
+  knife[:aws_account_id]         = "AWS_ACCOUNT_ID"
+end
