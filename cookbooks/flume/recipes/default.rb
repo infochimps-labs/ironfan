4ba4c5325b0196c2cf6f5be8ccfe46c955c80caf
Chris Howe <chris@howeville.com>
2011-02-10 15:34:22 -0600

    Flume masters needs to be in the same order on each machine. master_id should be the index of the current machine's ip in the masters list.

--- cookbooks/flume/recipes/default.rb
+++ cookbooks/flume/recipes/default.rb
@@ -10,19 +10,26 @@
 package "flume"
 
 flume_cluster = node[:flume][:cluster_name]
- 
+# All of the configs need to have the flume masters listed in the same order
+masters = all_provider_private_ips( "#{flume_cluster}-flume-master" ).sort
+
+# The master_id should be the index of the machine into the masters array
+master_id = masters.find_index( private_ip_of( node ) ) 
+
 template_vars = {
-  :master_id       => node[:node_name],
-  :masters         => all_provider_private_ips( "#{flume_cluster}-flume-master" ).join(","),
+  :master_id       => master_id,
+  :masters         => masters,
   :plugin_classes  => node[:flume][:plugin_classes],
   :flume_classpath => node[:flume][:classpath].join(":"),
   :zookeepers      => if node[:flume][:master][:external_zookeeper] then
-                          all_provider_private( "#{flume_cluster}->zookeeper-server" )
+                          all_provider_private_ips( "#{flume_cluster}-zookeeper" )
                         else
                           nil
                         end,
   :zookeeper_port => node[:flume][:zookeeper_port],
 }
+
+template_vars[:master_id]
 
 template "/etc/flume/conf/flume-site.xml" do
   source "flume-site.xml.erb"
