0f5192534aaaea148f31427dc4dfde14f3a39ea1
Chris Howe <chris@howeville.com>
2011-02-10 11:02:50 -0600

    Syntax error.

--- cookbooks/zookeeper/attributes/zookeeper.rb
+++ cookbooks/zookeeper/attributes/zookeeper.rb
@@ -3,4 +3,4 @@
 default[:groups]['zookeeper' ][:gid] = 305
 
 default[:zookeeper][:data_dir] = '/var/zookeeper'
-default[:zookeeper][:cluster_name= = node[:cluster_name]
+default[:zookeeper][:cluster_name] = node[:cluster_name]
