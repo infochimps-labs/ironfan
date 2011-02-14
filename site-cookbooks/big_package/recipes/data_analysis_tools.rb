3945209a1c7175e40b11d6568666b70d7f6ab964
Philip (flip) Kromer <flip@infochimps.org>
2011-02-14 13:51:33 -0500

    initial fiddling with a benchmark-to-qualify-nodes cookbook

--- site-cookbooks/big_package/recipes/data_analysis_tools.rb
+++ site-cookbooks/big_package/recipes/data_analysis_tools.rb
@@ -1,5 +1,6 @@
 
 %w[
-  x11-apps gimp eog texlive-common texlive-binaries dvipng ghostscript latex
+  r-base r-base-dev
+  x11-apps eog texlive-common texlive-binaries dvipng ghostscript latex
   libfreetype6 python-gtk2 python-gtk2-dev python-wxgtk2.8
 ].each{|pkg| package pkg }
