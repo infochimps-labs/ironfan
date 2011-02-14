91883cbbcf47d3c58ae1e8de13c63df709a2ba6a
Philip (flip) Kromer <flip@infochimps.org>
2011-02-14 14:49:25 -0500

    synced thrift repo *to* opscode_cookbooks

--- cookbooks/thrift/metadata.rb
+++ cookbooks/thrift/metadata.rb
@@ -12,3 +12,14 @@ supports "ubuntu"
 %w{ build-essential boost java subversion }.each do |cb|
   depends cb
 end
+
+
+attribute "thrift",
+  :display_name => "Thrift",
+  :description => "Hash of thrift attributes",
+  :type => "hash"
+
+attribute "thrift/repo_url",
+  :display_name => "Thrift subversion repository URL",
+  :description => "Location of the subversion repository to pull thrift from",
+  :default => "http://svn.apache.org/repos/asf/thrift"
