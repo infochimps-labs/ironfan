c34cecdef89eb0d26aa47708c62696a7822bff7e
Jacob Perkins <jacob.a.perkins@gmail.com>
2011-02-16 13:19:39 -0600

    simple recipe for installing a couple jruby gems

---
+++ site-cookbooks/jruby/recipes/gems.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: jruby
+# Recipe:: gems
+#
+# Copyright 2011, Infochimps, Inc.
+#
+#
+# Install basic gems
+#
+%w[json configliere erubis].each do |rubygem|
+  bash "install jruby version of #{rubygem}" do
+    user "root"
+    code <<-EOH
+      jruby -S gem install --no-ri --no-rdoc #{rubygem}
+    EOH
+  end
+end
