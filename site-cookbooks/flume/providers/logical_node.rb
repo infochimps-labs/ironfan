fa477f9d2638e1fc85f1ddc83123de8f5a64f89d
Chris Howe <chris@howeville.com>
2011-02-22 12:32:07 -0600

    made the spawn logic work with Flume 0.9.3-CDH3B4

--- site-cookbooks/flume/providers/logical_node.rb
+++ site-cookbooks/flume/providers/logical_node.rb
@@ -1,28 +1,33 @@
 require 'shellwords'
 
 
-
 def get_config(name,master)
   conf = `/usr/lib/flume/bin/flume shell -c #{master} -e getconfigs | grep #{name}`
   result = conf.strip.split(/\s*\t\s*/).map{|config| config.gsub(/\s+/,' ') } rescue ["","","",""]
   # name flow source sink
 end
 
-# This is an imperfect way to determine if the node is mapped
-def get_status(name,master)
-  status = `/usr/lib/flume/bin/flume shell -c #{master} -e getnodestatus | grep "#{name} --"`
-  if !status or status.empty? or status =~ /DECOMMISSIONED/
-    return :unmapped
-  else
-    return :mapped
+
+def get_mapping(name,master)
+  map = {}
+  mapping = `/usr/lib/flume/bin/flume shell -c #{master} -e getmappings`
+  mapping.split(/\n/).each do |s|
+    if s =~ /(\S+)\s+-->\s*\[(.*)\]/
+      physical = $1
+      logical  = $2.split(/,/)
+      logical.each {|l| map[l.strip] = physical } 
+    end
   end
+  map[name.strip]
 end
   
 action :config do
+  # figure out how this logical node is currently configured
   name,flow,source,sink = get_config( new_resource.name, new_resource.flume_master )
   my_name, my_flow, my_source, my_sink = 
     [ new_resource.name, new_resource.flow, new_resource.source, new_resource.sink ].map{ |config| config.gsub(/\s+/,' ' ) }
 
+  # only do a config if the configuration is different from the target
   if (my_flow != flow or my_source != source or my_sink != sink)  
     execute "configure logical node" do
       escaped_command = Shellwords.escape "exec config '#{new_resource.name}' '#{new_resource.flow}' '#{new_resource.source}' '#{new_resource.sink}'"    
@@ -34,7 +39,17 @@ action :config do
 end
 
 action :spawn do
-  if get_status( new_resource.name, new_resource.flume_master ) != :mapped
+  physical = get_mapping( new_resource.name, new_resource.flume_master )
+  Chef::Log.info "Logical node #{new_resource.name} -> #{physical.to_s}. Should be #{new_resource.physical_node }"
+  if( physical && physical != new_resource.physical_node )
+    execute "unmapping logical node #{new_resource.name}" do
+      escaped_command = Shellwords.escape "exec unmap '#{physical}' '#{new_resource.name}'"    
+      command "flume shell -c #{new_resource.flume_master} -e #{escaped_command}"
+    end
+    new_resource.updated_by_last_action(true)
+  end
+  
+  if ( physical != new_resource.physical_node )
     execute "spawn logical node" do
       escaped_command = Shellwords.escape "exec spawn '#{new_resource.physical_node}' '#{new_resource.name}'"    
       command "flume shell -c #{new_resource.flume_master} -e #{escaped_command}"
@@ -44,7 +59,8 @@ action :spawn do
 end
 
 action :unmap do
-  if get_status( new_resource.name, new_resource.flume_master ) != :mapped
+  physical = get_mapping( new_resource.name, new_resource.flume_master ) 
+  if physical == new_resource.physical_node
     execute "unmap logical node" do
       escaped_command = Shellwords.escape "exec unmap '#{new_resource.physical_node}' '#{new_resource.name}'"    
       command "flume shell -c #{new_resource.flume_master} -e #{escaped_command}"
