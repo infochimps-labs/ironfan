a77c768f2875402856d5112c582c5fc36dbf3bf1
Chris Howe <chris@howeville.com>
2011-03-02 22:19:28 -0600

    New cluster for scrapers

---
+++ clusters/flyingmonkeys.rb
@@ -0,0 +1,30 @@
+ClusterChef.cluster 'flyingmonkeys' do
+  merge!('defaults')
+  setup_role_implications
+
+  recipe                "hadoop_cluster::system_internals"
+  role                  "attaches_ebs_volumes"
+  role                  "nfs_client"
+  role                  "infochimps_base"
+  role                  "mounts_ebs_volumes"
+  role                  "benchmarkable"
+  role                  "big_package"
+  cloud do
+    backing             "ebs"
+    image_name          "infochimps-maverick-client"
+    user_data           :get_name_from => 'broham'
+  end
+
+  facet 'slave' do
+    instances           1
+    cloud.flavor        "t1.micro"
+  end
+
+  facet 'nikko' do
+    instances           1
+    cloud.flavor        "m2.xlarge"
+  end
+
+  chef_attributes({
+    })
+end
