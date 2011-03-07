bf84be4b7f703937b761ba611166e67d7c361e8c
Chris Howe <chris@howeville.com>
2011-03-07 14:39:54 -0600

    Added service definitions for flume-node and flume-master which will be restarted whenever the configuration changes.

--- site-cookbooks/flume/recipes/master.rb
+++ site-cookbooks/flume/recipes/master.rb
@@ -11,4 +11,8 @@ include_recipe "flume"
 
 package "flume-master"
 
+service "flume-master" do
+  supports :restart => true, :start=>true, :stop => true
+end
+
 provide_service ("#{node[:flume][:cluster_name]}-flume-master")
