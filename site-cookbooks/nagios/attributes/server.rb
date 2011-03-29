d74533dd7d622badc209f941b9264873899e3b0f
Chris Howe <chris@howeville.com>
2011-03-29 10:48:53 -0500

    Added logic to check health of elasticsearch

--- site-cookbooks/nagios/attributes/server.rb
+++ site-cookbooks/nagios/attributes/server.rb
@@ -27,12 +27,13 @@ set[:nagios][:state_dir] = "/var/lib/nagios3"
 set[:nagios][:docroot]   = "/usr/share/nagios3/htdocs"
 set[:nagios][:config_subdir] = "conf.d"
 
-default[:nagios][:notifications_enabled]   = 0
-default[:nagios][:check_external_commands] = true
-default[:nagios][:default_contact_groups]  = %w(admins)
-default[:nagios][:sysadmin_email]          = "root@localhost"
-default[:nagios][:sysadmin_sms_email]      = "root@localhost"
-default[:nagios][:server_auth_method]      = "openid"
+default[:nagios][:notifications_enabled]     = 0
+default[:nagios][:check_external_commands]   = true
+default[:nagios][:default_contact_groups]    = %w(admins)
+default[:nagios][:default_escalation_groups] = %w(admins)
+default[:nagios][:sysadmin_email]            = "root@localhost"
+default[:nagios][:sysadmin_sms_email]        = "root@localhost"
+default[:nagios][:server_auth_method]        = "openid"
 
 # This setting is effectively sets the minimum interval (in seconds) nagios can handle.
 # Other interval settings provided in seconds will calculate their actual from this value, since nagios works in 'time units' rather than allowing definitions everywhere in seconds
