648fd1a09045df534480d616d52f842b5ca1ae67
Mike Heffner <mikeh@fesnel.com>
2011-03-31 23:29:38 -0400

    Update cassandra cookbook to version 0.7.4.

--- site-cookbooks/cassandra/attributes/cassandra.rb
+++ site-cookbooks/cassandra/attributes/cassandra.rb
@@ -47,6 +47,8 @@ default[:cassandra][:jmx_port]                      = 12345         # moved from
 default[:cassandra][:auto_bootstrap]                = 'false'
 default[:cassandra][:authenticator]                 = "org.apache.cassandra.auth.AllowAllAuthenticator"
 default[:cassandra][:hinted_handoff_enabled]        = 'true'
+default[:cassandra][:max_hint_window_in_ms]         = 3600000
+default[:cassandra][:hinted_handoff_throttle_delay_in_ms] = 50
 default[:cassandra][:partitioner]                   = "org.apache.cassandra.dht.RandomPartitioner"       # "org.apache.cassandra.dht.OrderPreservingPartitioner"
 default[:cassandra][:endpoint_snitch]               = "org.apache.cassandra.locator.SimpleSnitch"
 default[:cassandra][:dynamic_snitch]                = 'true'
@@ -63,15 +65,19 @@ default[:cassandra][:memtable_flush_after]          = 60
 default[:cassandra][:sliced_buffer_size]            = 64            # size of column slices
 default[:cassandra][:thrift_framed_transport]       = 15            # default 15; fixes CASSANDRA-475, but make sure your client is happy (Set to nil for debugging)
 default[:cassandra][:thrift_max_message_length]     = 16
+default[:cassandra][:incremental_backups]           = false
 default[:cassandra][:snapshot_before_compaction]    = false
-default[:cassandra][:binary_memtable_throughput]    = 256
 default[:cassandra][:memtable_throughput]           = 64
 default[:cassandra][:memtable_ops]                  = 0.3
 default[:cassandra][:column_index_size]             = 64
 default[:cassandra][:in_memory_compaction_limit]    = 64
+default[:cassandra][:compaction_preheat_key_cache]  = true
 default[:cassandra][:commitlog_rotation_threshold]  = 128
 default[:cassandra][:commitlog_sync]                = "periodic"
 default[:cassandra][:commitlog_sync_period]         = 10000
+default[:cassandra][:flush_largest_memtables_at]    = 0.75
+default[:cassandra][:reduce_cache_sizes_at]         = 0.85
+default[:cassandra][:reduce_cache_capacity_to]      = 0.6
 default[:cassandra][:rpc_timeout_in_ms]             = 10000
 default[:cassandra][:rpc_keepalive]                 = "false"
 default[:cassandra][:phi_convict_threshold]         = 8
@@ -80,7 +86,7 @@ default[:cassandra][:throttle_limit]                = 80           # 2x (concurr
 default[:cassandra][:request_scheduler_id]          = 'keyspace'
 
 # For install_from_release recipe
-default[:cassandra][:install_url] = "http://apache.mirrors.tds.net/cassandra/0.7.0/apache-cassandra-0.7.0-beta2-bin.tar.gz"
+default[:cassandra][:install_url] = "http://www.eng.lsu.edu/mirrors/apache//cassandra/0.7.4/apache-cassandra-0.7.4-bin.tar.gz"
 # For install_from_git
 default[:cassandra][:git_repo]                      = 'git://git.apache.org/cassandra.git'
 # until ruby gem is updated, use cdd239dcf82ab52cb840e070fc01135efb512799
