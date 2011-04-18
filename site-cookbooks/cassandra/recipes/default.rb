3d5b29dffd920a1d8806598a8312da18b6ae13e3
Mike Heffner <mikeh@fesnel.com>
2011-04-18 09:38:03 -0400

    Move the saved_caches_dir to the configuration utility.

--- site-cookbooks/cassandra/recipes/default.rb
+++ site-cookbooks/cassandra/recipes/default.rb
@@ -45,6 +45,7 @@ directory('/ebs1/data'){ owner("root"); group("admin"); mode("0775"); action(:cr
 [ "/var/lib/cassandra", "/var/log/cassandra",
   node[:cassandra][:data_file_dirs],
   node[:cassandra][:commitlog_dir],
+  node[:cassandra][:saved_caches_dir]
 ].flatten.each do |cassandra_dir|
   directory cassandra_dir do
     owner    "cassandra"
