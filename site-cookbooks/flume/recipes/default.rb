4f0f892e58d0b2659e38037f7da4f01346640009
Chris Howe <chris@howeville.com>
2011-04-22 13:22:35 -0500

    Removed dangerous apt manipulations.

--- site-cookbooks/flume/recipes/default.rb
+++ site-cookbooks/flume/recipes/default.rb
@@ -11,19 +11,19 @@
 #include_recipe "apt"
 
 
-early = execute "add cloudera key" do
-  command "curl -s http://archive.cloudera.com/debian/archive.key | sudo apt-key add -"
-  action :nothing
-end
-
-early.run_action(:run)
-
-apt_repository "cloudera" do
-  uri "http://archive.cloudera.com/debian"
-  distribution "maverick-cdh3" 
-  components ["contrib"]
-  action :add
-end
+#early = execute "add cloudera key" do
+#  command "curl -s http://archive.cloudera.com/debian/archive.key | sudo apt-k#ey add -"
+#  action :nothing
+#end
+#
+#early.run_action(:run)
+#
+#apt_repository "cloudera" do
+#  uri "http://archive.cloudera.com/debian"
+#  distribution "maverick-cdh3" 
+#  components ["contrib"]
+#  action :add
+#end
  
 package "flume"
 
