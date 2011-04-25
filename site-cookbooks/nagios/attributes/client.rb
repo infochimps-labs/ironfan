390fd67295adf62fcfc36ff05b1145ff6bba3301
Chris Howe <chris@howeville.com>
2011-04-25 12:33:36 -0500

    Changed the way hadoop services are monitored.

--- site-cookbooks/nagios/attributes/client.rb
+++ site-cookbooks/nagios/attributes/client.rb
@@ -27,3 +27,4 @@ default[:nagios][:checks][:load][:warning]    = "15,10,5"
 default[:nagios][:checks][:smtp_host] = String.new
 
 default[:nagios][:server_role] = "monitoring"
+default[:nagios][:hostgroups_exclude] = []
