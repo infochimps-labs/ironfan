d9bb907fb84ef5268eb2e39a218b45988a3bd604
Mike Heffner <mikeh@fesnel.com>
2011-05-02 10:13:18 -0400

    Add cookbook to syslog to papertrailapp.com.

---
+++ site-cookbooks/papertrail/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Librato, Inc."
+maintainer_email "mike@librato.com"
+license          "All rights reserved"
+description      "Installs/Configures Sys Logging to papertrailapp.com"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
