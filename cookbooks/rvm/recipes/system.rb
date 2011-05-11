0785a93fd1753417689e500372d3f3674fbb67d3
Mike Heffner <mikeh@fesnel.com>
2011-05-11 15:38:17 -0400

    Import rvm cookbook.

---
+++ cookbooks/rvm/recipes/system.rb
@@ -0,0 +1,65 @@
+#
+# Cookbook Name:: rvm
+# Recipe:: system
+#
+# Copyright 2010, 2011 Fletcher Nichol
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# thanks to:
+# - http://www.agileweboperations.com/chef-rvm-ruby-enterprise-edition-as-default-ruby/
+# - http://github.com/denimboy/xprdev/blob/master/rvm/recipes/default.rb
+
+script_flags = ""
+if node[:rvm][:version]
+  script_flags += " --version #{node[:rvm][:version]}"
+end
+if node[:rvm][:branch]
+  script_flags += " --branch #{node[:rvm][:branch]}"
+end
+
+pkgs = %w{ sed grep tar gzip bzip2 bash curl }
+case node[:platform]
+  when "centos","redhat","fedora"
+    pkgs << "git"
+  when "debian","ubuntu","suse"
+    pkgs << "git-core"
+end
+
+pkgs.each do |pkg|
+  package pkg
+end
+
+execute "install system-wide RVM" do
+  user      "root"
+  command   <<-CODE
+    bash -c "bash <( curl -Ls #{node[:rvm][:installer_url]} )#{script_flags}"
+  CODE
+  not_if    rvm_wrap_cmd(%{type rvm | head -1 | grep -q '^rvm is a function$'})
+end
+
+template  "/etc/rvmrc" do
+  source  "rvmrc.erb"
+  owner   "root"
+  group   "rvm"
+  mode    "0644"
+end
+
+execute "upgrade RVM to #{node[:rvm][:upgrade]}" do
+  user      "root"
+  command   rvm_wrap_cmd(%{rvm get #{node[:rvm][:upgrade]}})
+  only_if do
+    %{ latest head }.include? node[:rvm][:upgrade]
+  end
+end
