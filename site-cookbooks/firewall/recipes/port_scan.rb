4de069ccb6e05d65012f87d874eec34b37753583
Mike Heffner <mikeh@fesnel.com>
2011-05-12 12:33:51 -0400

    Use --rttl to prevent attacks and use a NAME for the rule.

--- site-cookbooks/firewall/recipes/port_scan.rb
+++ site-cookbooks/firewall/recipes/port_scan.rb
@@ -12,9 +12,14 @@ include_recipe 'iptables'
 (node[:firewall] || {}).keys.each do |k|
   m = k.to_s.match(/^port_scan_(.*)/)
   if m
+    Chef::Log.info("OPTIONS: #{(node[:firewall][k]).merge(:name => m[1]).inspect}, node: #{(node[:firewall][k]).inspect}")
     iptables_rule "no_port_scan_#{m[1]}" do
       source "no_port_scan.erb"
-      variables(node[:firewall][k])
+      variables({ :port => node[:firewall][k][:port],
+                  :max_conns => node[:firewall][k][:max_conns],
+                  :window => node[:firewall][k][:window],
+                  :name => m[1]
+                })
     end
   end
 end
