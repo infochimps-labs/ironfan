5b7e0a17b8837295dadc17bc0f9498353a11e99b
Philip (flip) Kromer <flip@infochimps.org>
2011-05-14 15:25:28 -0500

    merged from howech 8eb04f6dd

--- site-cookbooks/zookeeper/attributes/zookeeper.rb
+++ site-cookbooks/zookeeper/attributes/zookeeper.rb
@@ -5,3 +5,4 @@ default[:groups]['zookeeper' ][:gid] = 305
 default[:zookeeper][:data_dir] = '/var/zookeeper'
 default[:zookeeper][:cluster_name] = node[:cluster_name]
 default[:zookeeper][:log_dir] = '/var/log/zookeeper'
+default[:zookeeper][:max_client_connections] = 30
