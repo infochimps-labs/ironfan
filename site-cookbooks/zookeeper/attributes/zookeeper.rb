6c55256ecc53ad5f6feceaaa68e568d6ac9ebf17
Philip (flip) Kromer <flip@infochimps.org>
2011-05-14 15:10:34 -0500

    merge 443add733f67b4

--- site-cookbooks/zookeeper/attributes/zookeeper.rb
+++ site-cookbooks/zookeeper/attributes/zookeeper.rb
@@ -3,3 +3,5 @@
 default[:groups]['zookeeper' ][:gid] = 305
 
 default[:zookeeper][:data_dir] = '/var/zookeeper'
+default[:zookeeper][:cluster_name] = node[:cluster_name]
+default[:zookeeper][:log_dir] = '/var/log/zookeeper'
