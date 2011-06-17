3bd07600e255dc290025ebbe474dde23af8ca0c1
Chris Howe <chris@howeville.com>
2011-06-17 13:44:41 -0500

    Added chimpmark cluster Minor change to sandbox cluster

---
+++ clusters/chimpmark.rb
@@ -0,0 +1,24 @@
+ClusterChef.cluster 'chimpmark' do
+  use :defaults
+  setup_role_implications
+
+  cloud do
+    backing             "instance"
+    image_name          "infochimps-maverick-client"
+    region              "us-east"
+  end
+
+  facet 'master' do
+    instances           1
+    cloud.flavor        "m1.xlarge"
+  end
+
+  facet 'slave' do
+    instances           1
+    cloud.flavor        "m1.xlarge"
+  end
+
+  chef_attributes({
+  })
+
+end
