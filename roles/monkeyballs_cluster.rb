ce6e5f3a6bd4df5108f332aa6b94dcbefbce6400
Philip (flip) Kromer <flip@infochimps.org>
2011-07-01 10:49:05 -0500

    attributes for throwaway cluster

--- roles/monkeyballs_cluster.rb
+++ roles/monkeyballs_cluster.rb
@@ -1,2 +1,13 @@
 name        'monkeyballs_cluster'
 description 'Holds cluster-wider overrides and recipes for the monkeyballs cluster'
+
+# Force these attributes overriding all else
+override_attributes({
+    :cluster_size => 3,
+
+    :hadoop => {
+      :max_map_tasks    => 6,
+      :max_reduce_tasks => 2,
+      :cdh_version =>  "cdh3",
+    },
+  })
