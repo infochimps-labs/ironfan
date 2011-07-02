3d2c28ba5ace5557839f58f16537d16eb5e1ec8a
Philip (flip) Kromer <flip@infochimps.org>
2011-07-02 17:50:46 -0500

    added granular heapsize settings for namenode, 2nn, jt. Also can pin down the distro name to use when hitting cloudera's apt repo

--- roles/monkeyballs_cluster.rb
+++ roles/monkeyballs_cluster.rb
@@ -10,11 +10,19 @@ override_attributes({
       :max_map_tasks    => 16,
       :max_reduce_tasks => 2,
       :cdh_version =>  "cdh3",
+      :cdh_version   => 'cdh3',
+      :deb_version   => "0.20.2+923.21-1~maverick-cdh3",
 
       :hadoop_daemon_heapsize            => 1000,
       :hadoop_namenode_heapsize          => 4000,
       :hadoop_secondarynamenode_heapsize => 4000,
       :hadoop_jobtracker_heapsize        => 4000,
-
     },
+    :java => {
+      :install_flavor => 'sun'
+    },
+    :ruby => {
+      :version => '1.9.1', # this installs 1.9.2 (no, really)
+    }
   })
+
--- site-cookbooks/hadoop_cluster/attributes/hadoop_cluster.rb
+++ site-cookbooks/hadoop_cluster/attributes/hadoop_cluster.rb
@@ -2,6 +2,7 @@
 default[:hadoop][:hadoop_handle] = 'hadoop-0.20'
 default[:hadoop][:cdh_version]   = 'cdh3b3'
 default[:hadoop][:deb_version]   = "0.20.2+737-1~lucid-cdh3b3"
+default[:hadoop][:cloudera_distro_name] = nil # in case cloudera doesn't have you distro yet
 
 # Make sure you define a cluster_size in roles/WHATEVER_cluster.rb
 default[:cluster_size] = 5
