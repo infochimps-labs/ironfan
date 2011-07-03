74c99410a560c013c3c530d934522d316916cddf
Philip (flip) Kromer <flip@infochimps.org>
2011-07-03 01:49:48 -0500

    moved the nodename script into cluster_chef cookbook

--- site-cookbooks/base/recipes/cleanup.rb
+++
@@ -1,6 +0,0 @@
-#
-execute 'updatedb' do
-  command %Q{updatedb}
-  creates '/var/lib/mlocate/mlocate.db'
-end
-
--- site-cookbooks/cluster_chef/recipes/dedicated_server_tuning.rb
+++ site-cookbooks/cluster_chef/recipes/dedicated_server_tuning.rb
@@ -2,7 +2,6 @@ overcommit_memory  =     1
 overcommit_ratio   =   100
 ulimit_hard_nofile = 32768
 ulimit_soft_nofile = 32768
-fs_epoll_max_user_instances = 4096
 
 def set_proc_sys_limit desc, proc_path, limit
   bash desc do
@@ -14,29 +13,17 @@ end
 set_proc_sys_limit "VM overcommit ratio", '/proc/sys/vm/overcommit_memory', overcommit_memory
 set_proc_sys_limit "VM overcommit memory", '/proc/sys/vm/overcommit_ratio',  overcommit_ratio
 
-# recent kernels do away with this limit, so these lines aren't necessary.
-# # http://pero.blogs.aprilmayjune.org/2009/01/22/hadoop-and-linux-kernel-2627-epoll-limits/
-# # https://patchwork.kernel.org/patch/4548/
-# set_proc_sys_limit "Increase epoll instances limit", '/proc/sys/fs/epoll/max_user_instances', fs_epoll_max_user_instances
-
-bash "Increase open files hard ulimit for @hadoop group" do
-  not_if "egrep -q 'hbase.*hard.*nofile.*#{ulimit_hard_nofile}' /etc/security/limits.conf"
-  code <<EOF
-    egrep -q '@hadoop.*hard.*nofile' || ( echo '@hadoop hard nofile' >> /etc/security/limits.conf )
-    sed -i "s/@hadoop.*hard.*nofile.*/@hadoop    hard    nofile  #{ulimit_hard_nofile}/" /etc/security/limits.conf
-    egrep -q 'hbase.*hard.*nofile' || ( echo 'hbase   hard nofile' >> /etc/security/limits.conf )
-    sed -i "s/hbase.*hard.*nofile.*/hbase      hard    nofile  #{ulimit_hard_nofile}/" /etc/security/limits.conf
+%w[ @hadoop @elasticsearch hbase ].each do |usr|
+  { 'hard' => ulimit_hard_nofile, 'soft' => ulimit_soft_nofile,  }.each do |limit_type, limit|
+    bash "Increase open files #{limit_type} ulimit for #{usr} group" do
+      not_if "egrep -q '#{usr}.*#{limit_type}.*nofile.*#{limit}' /etc/security/limits.conf"
+      code <<EOF
+        egrep -q '#{usr}.*#{limit_type}.*nofile' || ( echo '#{usr} #{limit_type} nofile' >> /etc/security/limits.conf )
+        sed -i "s/#{usr}.*#{limit_type}.*nofile.*/#{usr}    #{limit_type}    nofile  #{limit}/" /etc/security/limits.conf
 EOF
+    end
+  end
 end
 
-bash "Increase open files soft ulimit for @hadoop group" do
-  not_if "egrep -q '@hadoop.*soft.*nofile.*#{ulimit_soft_nofile}' /etc/security/limits.conf"
-  code <<EOF
-    egrep -q '@hadoop.*soft.*nofile' || ( echo '@hadoop soft nofile' >> /etc/security/limits.conf )
-    sed -i "s/@hadoop.*soft.*nofile.*/@hadoop    soft    nofile  #{ulimit_soft_nofile}/" /etc/security/limits.conf
-    egrep -q 'hbase.*soft.*nofile' || ( echo 'hbase   soft nofile' >> /etc/security/limits.conf )
-    sed -i "s/hbase.*soft.*nofile.*/hbase      soft    nofile  #{ulimit_soft_nofile}/" /etc/security/limits.conf
-EOF
-end
 
 
