ede70b1331720eb3077e912d8a3fa70f0548a9ab
Philip (flip) Kromer <flip@infochimps.org>
2011-07-17 21:42:36 -0500

    cluster_service_discovery dependency

--- site-cookbooks/jenkins/metadata.rb
+++ site-cookbooks/jenkins/metadata.rb
@@ -4,4 +4,4 @@ license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.5"
-%w(runit java iptables).each { |cb| depends cb }
+%w(runit java iptables cluster_service_discovery).each { |cb| depends cb }
