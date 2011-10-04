0d6b0515e7e8db3c30698153e65fef6652dc0475
Philip (flip) Kromer <flip@infochimps.org>
2011-10-04 13:34:48 -0500

    Map/R recipes

---
+++ site-cookbooks/map_r/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Philip (flip) Kromer"
+maintainer_email "flip@infochimps.com"
+license          "All rights reserved"
+description      "Installs/Configures map_r"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ site-cookbooks/map_r/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: map_r
+# Recipe:: default
+#
+# Copyright 2011, Philip (flip) Kromer
+#
+# All rights reserved - Do Not Redistribute
+#
