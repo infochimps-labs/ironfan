dc0b5846ae56b51721e0d76e7e89adaafad006cc
Dhruv Bansal <dhruv@ph.utexas.edu>
2011-11-04 12:13:38 -0500

    reordered zabbix::agent_source recipe so that everything happens in the proper order --> zabbix_agentd will start properly the *first* time

--- cookbooks/zabbix/recipes/agent_source.rb
+++ cookbooks/zabbix/recipes/agent_source.rb
@@ -19,13 +19,36 @@ when "centos"
   log "No centos Support yet"
 end
 
+
+# Download zabbix source code
+remote_file "/opt/zabbix-#{node.zabbix.agent.version}.tar.gz" do
+  source "http://freefr.dl.sourceforge.net/project/zabbix/#{node.zabbix.agent.branch}/#{node.zabbix.agent.version}/zabbix-#{node.zabbix.agent.version}.tar.gz"
+  mode "0644"
+  action :create_if_missing
+  notifies :run, "script[install_zabbix_agent]"
+end
+
+# installation of zabbix bin
+script "install_zabbix_agent" do
+  interpreter "bash"
+  user "root"
+  cwd "/opt"
+  action :nothing
+  notifies :restart, "service[zabbix_agentd]", :delayed
+  code <<-EOH
+  tar xvfz zabbix-#{node.zabbix.agent.version}.tar.gz
+  (cd zabbix-#{node.zabbix.agent.version} && ./configure --enable-agent #{node.zabbix.agent.configure_options.join(" ")})
+  (cd zabbix-#{node.zabbix.agent.version} && make install)
+  EOH
+end
+
 # Install configuration
 template "/etc/zabbix/zabbix_agentd.conf" do
   source "zabbix_agentd.conf.erb"
   owner "root"
   group "root"
   mode "644"
-  notifies :restart, "service[zabbix_agentd]"
+  notifies :restart, "service[zabbix_agentd]", :delayed
 end
 
 # Install Init script
@@ -34,29 +57,6 @@ template "/etc/init.d/zabbix_agentd" do
   owner "root"
   group "root"
   mode "754"
-end
-
-
-# installation of zabbix bin
-script "install_zabbix_agent" do
-  interpreter "bash"
-  user "root"
-  cwd "/opt"
-  action :nothing
-  notifies :restart, "service[zabbix_agentd]"
-  code <<-EOH
-  tar xvfz zabbix-#{node.zabbix.agent.version}.tar.gz
-  (cd zabbix-#{node.zabbix.agent.version} && ./configure --enable-agent #{node.zabbix.agent.configure_options.join(" ")})
-  (cd zabbix-#{node.zabbix.agent.version} && make install)
-  EOH
-end
-
-# Download zabbix source code
-remote_file "/opt/zabbix-#{node.zabbix.agent.version}.tar.gz" do
-  source "http://freefr.dl.sourceforge.net/project/zabbix/#{node.zabbix.agent.branch}/#{node.zabbix.agent.version}/zabbix-#{node.zabbix.agent.version}.tar.gz"
-  mode "0644"
-  action :create_if_missing
-  notifies :run, "script[install_zabbix_agent]"
 end
 
 # Define zabbix_agentd service
