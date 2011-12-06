20168dcd5a479d6c25b27284172d7a6305fa4b72
Nathaniel Eliot <temujin9@t9productions.com>
2011-12-06 10:58:20 -0600

    Adding prerequisites to cluster_chef metacookbook libraries

--- meta-cookbooks/cluster_chef/libraries/cluster_chef.rb
+++ meta-cookbooks/cluster_chef/libraries/cluster_chef.rb
@@ -4,6 +4,9 @@
 #
 require File.expand_path('attr_struct.rb', File.dirname(__FILE__))
 require File.expand_path('node_utils.rb', File.dirname(__FILE__))
+require File.expand_path('aspect.rb', File.dirname(__FILE__))
+require File.expand_path('aspects.rb', File.dirname(__FILE__))
+require File.expand_path('discovery.rb', File.dirname(__FILE__))
 
 # FIXME -- remove:
 require 'pry' ; require 'ap'
