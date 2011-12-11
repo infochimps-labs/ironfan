740291b3d7d692cf12a49583806e6b9d3846a773
Philip (flip) Kromer <flip@infochimps.org>
2011-12-10 18:10:25 -0600

    fighting with jeweler over where to push things

--- tasks/jeweler_use_alt_branch.rb
+++ tasks/jeweler_use_alt_branch.rb
@@ -1,24 +1,47 @@
 
 #
-#
+# Jeweler has hardcoded the 'master' branch as where to push from.
+# We hardcode it right back in.
 #
 
-Jeweler::Commands::ReleaseToGit.class_eval do
-  def run
-    unless clean_staging_area?
-      system "git status"
-      raise "Unclean staging area! Be sure to commit or .gitignore everything first. See `git status` above."
+module Jeweler::Commands
+
+  PUSH_FROM_BRANCH = 'version_3' unless defined?(PUSH_FROM_BRANCH)
+  
+  ReleaseToGit.class_eval do
+    def run
+      unless clean_staging_area?
+        system "git status"
+        raise "Unclean staging area! Be sure to commit or .gitignore everything first. See `git status` above."
+      end
+
+      repo.checkout(PUSH_FROM_BRANCH)
+      repo.push
+
+      if release_not_tagged?
+        output.puts "Tagging #{release_tag}"
+        repo.add_tag(release_tag)
+
+        output.puts "Pushing #{release_tag} to origin"
+        repo.push('origin', release_tag)
+      end
     end
+  end
+  
+  ReleaseGemspec.class_eval do
+    def run
+      unless clean_staging_area?
+        system "git status"
+        raise "Unclean staging area! Be sure to commit or .gitignore everything first. See `git status` above."
+      end
 
-    repo.checkout('version_3')
-    repo.push
+      repo.checkout(PUSH_FROM_BRANCH)
 
-    if release_not_tagged?
-      output.puts "Tagging #{release_tag}"
-      repo.add_tag(release_tag)
+      regenerate_gemspec!
+      commit_gemspec! if gemspec_changed?
 
-      output.puts "Pushing #{release_tag} to origin"
-      repo.push('origin', release_tag)
+      output.puts "Pushing #{PUSH_FROM_BRANCH} to origin"
+      repo.push
     end
   end
 end
