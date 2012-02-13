881025c9adde02494d7e347d8bce550f8c315480
Philip (flip) Kromer <flip@infochimps.org>
2012-02-13 17:46:16 -0600

    oops printf left in dammit

--- lib/cluster_chef/compute.rb
+++ lib/cluster_chef/compute.rb
@@ -73,7 +73,6 @@ module ClusterChef
     def raid_group(rg_name, attrs={}, &block)
       volumes[rg_name] ||= ClusterChef::RaidGroup.new(:parent => self, :name => rg_name)
       volumes[rg_name].configure(attrs, &block)
-      p volumes
       volumes[rg_name].sub_volumes.each do |sv_name|
         volume(sv_name){ in_raid(rg_name) ; mountable(false) ; tags({}) }
       end
