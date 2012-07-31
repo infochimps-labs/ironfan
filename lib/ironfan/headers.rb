b989abb4f8480ffb1692fbfa8093bc8783bb5d79
Nathaniel Eliot <temujin9@t9productions.com>
2012-07-31 11:52:15 -0500

    Beginning framework for Storage (abstract collection of Dsl::Volume, chef attributes, and Provider::Volume, like Machine does for other instance resources)

--- lib/ironfan/headers.rb
+++ lib/ironfan/headers.rb
@@ -11,6 +11,8 @@ module Ironfan
   class Broker < Builder
     class Machine < Builder; end
     class Machines < Gorillib::ModelCollection; end
+    class Storage < Builder; end
+    class Storages < Gorillib::ModelCollection; end
   end
 
   class Dsl < Builder
--- lib/ironfan/requirements.rb
+++ lib/ironfan/requirements.rb
@@ -45,6 +45,7 @@ require 'ironfan/provider/virtualbox/instance'
 # Broker classes to coordinate DSL expectations and provider resources
 require 'ironfan/broker'
 require 'ironfan/broker/machine'
+require 'ironfan/broker/storage'
 
 
 # Calls that are slated to go away
