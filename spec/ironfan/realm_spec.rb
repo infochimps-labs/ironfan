9d9f30efebc59acfefb3972c75fb7370d5e5083d
Josh Bronson <josh.bronson@gmail.com>
2013-11-05 16:29:56 -0600

    more tests for realm cloud settings

--- spec/ironfan/realm_spec.rb
+++ spec/ironfan/realm_spec.rb
@@ -19,4 +19,58 @@ describe Ironfan::Dsl::Realm do
     Ironfan.cluster(:foo_baz).facets.to_a.should_not(be_empty)
     Ironfan.cluster(:foo_baz).servers.to_a.first.should_not(be_nil)
   end
+
+  it 'should create clusters with names prefixed by its own' do
+    Ironfan.realm(:foo){ cluster(:bar) }
+    Ironfan.cluster(:foo_bar).should_not(be_nil)
+  end
+
+  it 'should create clusters with machines' do
+    Ironfan.realm(:foo){ cluster(:bar) }
+    Ironfan.cluster(:foo_bar).should_not(be_nil)
+  end
+
+  it 'should create clusters with the correct ssh user' do
+    (Chef::Config[:ec2_image_info] ||= {}).merge!({
+      %w[us-east-1  64-bit  ebs     ironfan-precise  ] =>
+      { :image_id => 'ami-29fe7640', :ssh_user => 'bam', :bootstrap_distro => "ubuntu12.04-ironfan", },
+    })
+    Ironfan.realm(:foo) do
+      cloud(:ec2) do
+        flavor 'm1.xlarge'
+        image_name 'ironfan-precise'
+      end
+      cluster(:bar){ facet(:baz) }
+    end
+    cloud = Ironfan.cluster(:foo_bar).facets[:baz].servers.to_a.first.cloud(:ec2)
+    cloud.flavor.should == 'm1.xlarge'
+    cloud.ssh_user.should == 'bam'
+  end
+
+  it 'should save cloud properties to be shared among all clusters within the realm' do
+    Ironfan.realm :foo do
+      cloud(:ec2).flavor 'm1.xlarge'
+      cluster(:bar){ facet(:baz) }
+    end
+
+    # We need to resolve before the cloud settings come through
+    Ironfan.realm(:foo).clusters[:foo_bar].resolve.facets[:baz].cloud(:ec2).flavor.should == 'm1.xlarge'
+
+    # Ironfan.cluster will do the resolution for us.
+    Ironfan.cluster(:foo_bar).facets[:baz].cloud(:ec2).flavor.should == 'm1.xlarge'
+  end
+
+  it 'should save an environment to be shared among all clusters within the realm' do
+    Ironfan.realm :foo do
+      environment :bif
+      cluster(:bar){ facet(:baz) }
+    end
+
+    # We need to resolve before the cloud settings come through
+    Ironfan.realm(:foo).clusters[:foo_bar].resolve.facets[:baz].environment.should == :bif
+
+    # Ironfan.cluster will do the resolution for us.
+    Ironfan.cluster(:foo_bar).facets[:baz].environment.should == :bif
+  end
+  
 end
