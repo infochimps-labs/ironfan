47379f6c2a8d636937eb9f879200507569a7e34d
Josh Bronson <josh.bronson@gmail.com>
2013-11-06 10:20:56 -0600

    refactor realm spec

--- spec/ironfan/realm_spec.rb
+++ spec/ironfan/realm_spec.rb
@@ -3,44 +3,20 @@ require 'spec_helper'
 require 'ironfan'
 
 describe Ironfan::Dsl::Realm do
-  it 'should create clusters that can be referenced later' do
-    x = self
-    Ironfan.realm :foo do
-      cluster(:bar).should(x.be(cluster(:bar)))
-    end
-  end
-
-  it 'should create clusters that can be edited later' do
-    Ironfan.realm :foo do
-      cluster(:baz)
-      cluster(:baz){ facet :bif }
-    end
-
-    Ironfan.cluster(:foo_baz).facets.to_a.should_not(be_empty)
-    Ironfan.cluster(:foo_baz).servers.to_a.first.should_not(be_nil)
-  end
-
-  it 'should create clusters with names prefixed by its own' do
-    Ironfan.realm(:foo){ cluster(:bar) }
-    Ironfan.cluster(:foo_bar).should_not(be_nil)
-  end
-
-  it 'should create clusters with machines' do
-    Ironfan.realm(:foo){ cluster(:bar){ facet(:baz).instances 1 }}
-    Ironfan.cluster(:foo_bar).facets[:baz].server(0).should_not(be_nil)
-  end
-
-  it 'should create clusters with attributes correctly applied' do
+  before(:each) do
     (Chef::Config[:ec2_image_info] ||= {}).merge!({
       %w[us-east-1  64-bit  ebs     ironfan-precise  ] =>
       { :image_id => 'ami-29fe7640', :ssh_user => 'bam', :bootstrap_distro => "ubuntu12.04-ironfan", },
     })
+
     Ironfan.realm(:foo) do
+      environment :bif
+
       cloud(:ec2) do
         flavor 'm1.xlarge'
         image_name 'ironfan-precise'
       end
-        
+      
       cluster(:bar) do
         cluster_role.override_attributes(a: 1)
         facet(:baz) do
@@ -50,6 +26,34 @@ describe Ironfan::Dsl::Realm do
         end
       end
     end
+  end
+
+  it 'should create clusters that can be referenced later' do
+    x = self
+    Ironfan.realm :xx do
+      cluster(:bar).should(x.be(cluster(:bar)))
+    end
+  end
+
+  it 'should create clusters that can be edited later' do
+    Ironfan.realm :xy do
+      cluster(:baz)
+      cluster(:baz){ facet :bif }
+    end
+
+    Ironfan.cluster(:xy_baz).facets.to_a.should_not(be_empty)
+    Ironfan.cluster(:xy_baz).servers.to_a.first.should_not(be_nil)
+  end
+
+  it 'should create clusters with names prefixed by its own' do
+    Ironfan.cluster(:foo_bar).should_not(be_nil)
+  end
+
+  it 'should create clusters with machines' do
+    Ironfan.cluster(:foo_bar).facets[:baz].server(0).should_not(be_nil)
+  end
+
+  it 'should create clusters with attributes correctly applied' do
     manifest = Ironfan.cluster(:foo_bar).facets[:baz].server(0).to_machine_manifest
     manifest.cluster_override_attributes.should == {a: 1}
     manifest.facet_override_attributes.should == {b: 1}
@@ -57,35 +61,12 @@ describe Ironfan::Dsl::Realm do
   end
 
   it 'should create clusters with the correct ssh user' do
-    (Chef::Config[:ec2_image_info] ||= {}).merge!({
-      %w[us-east-1  64-bit  ebs     ironfan-precise  ] =>
-      { :image_id => 'ami-29fe7640', :ssh_user => 'bam', :bootstrap_distro => "ubuntu12.04-ironfan", },
-    })
-    Ironfan.realm(:foo) do
-      cloud(:ec2) do
-        flavor 'm1.xlarge'
-        image_name 'ironfan-precise'
-      end
-      cluster(:bar){ facet(:baz) }
-    end
     manifest = Ironfan.cluster(:foo_bar).facets[:baz].servers.to_a.first.to_machine_manifest
     manifest.flavor.should == 'm1.xlarge'
     manifest.ssh_user.should == 'bam'
   end
 
   it 'should save cloud properties to be shared among all clusters within the realm' do
-    (Chef::Config[:ec2_image_info] ||= {}).merge!({
-      %w[us-east-1  64-bit  ebs     ironfan-precise  ] =>
-      { :image_id => 'ami-29fe7640', :ssh_user => 'bam', :bootstrap_distro => "ubuntu12.04-ironfan", },
-    })
-    Ironfan.realm :foo do
-      cloud(:ec2) do
-        flavor 'm1.xlarge'
-        image_name 'ironfan-precise'
-      end        
-      cluster(:bar){ facet(:baz) }
-    end
-
     # We need to resolve before the cloud settings come through
     Ironfan.realm(:foo).clusters[:foo_bar].resolve.facets[:baz].servers.to_a.first.to_machine_manifest.flavor.should == 'm1.xlarge'
 
@@ -94,16 +75,10 @@ describe Ironfan::Dsl::Realm do
   end
 
   it 'should save an environment to be shared among all clusters within the realm' do
-    Ironfan.realm :foo do
-      environment :bif
-      cluster(:bar){ facet(:baz) }
-    end
-
     # We need to resolve before the cloud settings come through
     Ironfan.realm(:foo).clusters[:foo_bar].resolve.facets[:baz].environment.should == :bif
 
     # Ironfan.cluster will do the resolution for us.
     Ironfan.cluster(:foo_bar).facets[:baz].environment.should == :bif
   end
-  
 end
