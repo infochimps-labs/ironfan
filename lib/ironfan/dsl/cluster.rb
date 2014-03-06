0029c44227308793a76a53610bf8cdcff4bc83c2
Travis Dempsey <travis@infochimps.com>
2014-03-06 16:03:36 -0600

    create a factory method and registry for clusters
    Change-Id: I1cc0b9c982069631d842df796db66a250f311350

--- lib/ironfan/dsl/cluster.rb
+++ lib/ironfan/dsl/cluster.rb
@@ -1,35 +1,45 @@
 module Ironfan
   class Dsl
-
     class Cluster < Ironfan::Dsl::Compute
-      collection :facets,       Ironfan::Dsl::Facet,   :resolver => :deep_resolve
       include Ironfan::Plugin::Base; register_with Ironfan::Dsl::Realm
+
+      collection :facets, Ironfan::Dsl::Facet, resolver: :deep_resolve
+
+      def self.plugin_hook(owner, attrs, plugin_name, full_name, &blk)
+        owner.cluster(plugin_name, new(attrs.merge(name: full_name, owner: owner)))
+        _project(cluster, &blk)
+      end
+
+      def self.definitions
+        @clusters ||= {}
+      end
+
+      def self.define(attrs = {}, &blk)
+        cl = new(attrs)
+        cl.receive!({}, &blk) # the ordering of the initialize method is super fragile
+        definitions[attrs[:name].to_sym] = cl
+      end
+      
+      def initialize(attrs = {}, &blk)
+        super
+        self.cluster_role  Ironfan::Dsl::Role.new(name: "#{attrs[:name]}-cluster")
+        self.realm_name    attrs[:owner].name          unless attrs[:owner].nil?
+        self.cluster_names attrs[:owner].cluster_names unless attrs[:owner].nil?
+      end
+
+      # Utility method to reference all servers from constituent facets
+      def servers
+        result = Gorillib::ModelCollection.new(item_type: Ironfan::Dsl::Server, key_method: :full_name)
+        facets.each{ |f| f.servers.each{ |s| result << s } }
+        result
+      end
 
       def children
         facets.to_a + components.to_a
       end
 
-      def initialize(attrs={},&block)
-        super
-        self.cluster_role       Ironfan::Dsl::Role.new(:name => "#{attrs[:name]}-cluster")
-        self.realm_name         attrs[:owner].name unless attrs[:owner].nil?
-        self.cluster_names      attrs[:owner].cluster_names unless attrs[:owner].nil?
-      end
-
-      # Utility method to reference all servers from constituent facets
-      def servers
-        result = Gorillib::ModelCollection.new(:item_type => Ironfan::Dsl::Server, :key_method => :full_name)
-        facets.each {|f| f.servers.each {|s| result << s} }
-        result
-      end
-
       def cluster_name
         name
-      end
-
-      def self.plugin_hook owner, attrs, plugin_name, full_name, &blk
-        owner.cluster(plugin_name, new(attrs.merge(name: full_name, owner: owner)))
-        _project cluster, &blk
       end
     end
   end
