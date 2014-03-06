7cb8dfb357a8f62754a0365ade7885521a9011e5
Travis Dempsey <travis@infochimps.com>
2014-03-06 16:10:07 -0600

    major overhaul of the ironfan module methods
    Change-Id: I56a79b375aa885129c694844246060b377e6a411

--- lib/ironfan.rb
+++ lib/ironfan.rb
@@ -3,39 +3,109 @@ require 'ironfan/requirements'
 module Ironfan
   module_function
 
-  @@clusters ||= Hash.new
-  @@realms   ||= Hash.new
-
-  # path to search for cluster definition files
-  def cluster_path
-    return Array(Chef::Config[:cluster_path]) if Chef::Config[:cluster_path]
-    raise "Holy smokes, you have no cookbook_path or cluster_path set up. Follow chef's directions for creating a knife.rb." if Chef::Config[:cookbook_path].blank?
-    cl_path = Chef::Config[:cookbook_path].map{|dir| File.expand_path('../clusters', dir) }.uniq
-    ui.warn "No cluster path set. Taking a wild guess that #{cl_path.inspect} is \nreasonable based on your cookbook_path -- but please set cluster_path in your knife.rb"
-    Chef::Config[:cluster_path] = cl_path
-  end
-
   #
-  # Delegates
+  # Attributes
+  #
   def clusters
-    @@clusters
+    Ironfan::Dsl::Cluster.definitions
+  end
+
+  def realms
+    Ironfan::Dsl::Realm.definitions
+  end
+
+  def ui=(ui)           @ui = ui           ; end
+  def ui()              @ui                ; end
+
+  def chef_config=(cc)  @chef_config = cc  ; end
+  def chef_config()     @chef_config       ; end
+
+  def knife_config=(kc) @knife_config = kc ; end
+  def knife_config()    @knife_config      ; end
+
+  #
+  # Dsl constructors
+  #
+  def cluster(name, attrs = {}, &blk)
+    existing = clusters[name.to_sym]
+    return existing if attrs.empty? && !block_given?
+    if existing
+      existing.receive!(attrs, &blk)
+      # existing.resolve!
+    else
+      cl = Ironfan::Dsl::Cluster.define(attrs.merge(name: name.to_sym), &blk)
+      # cl.resolve!
+    end
+  end
+
+  def realm(name, attrs = {}, &blk)
+    existing = realms[name.to_sym]
+    return existing if attrs.empty? && !block_given?
+    if existing
+      existing.receive!(attrs, &blk)
+      # existing.resolve!
+    else
+      rlm = Ironfan::Dsl::Realm.define(attrs.merge(name: name.to_sym), &blk)
+      # rlm.resolve!
+    end
   end
 
   #
-  # Delegates
-  def realms
-    @@realms
+  # Dsl loaders
+  #
+  def clusters_dir
+    return Array(chef_config[:cluster_path]) if chef_config[:cluster_path]
+    raise "Holy smokes, you have no cookbook_path or cluster_path set up. Follow chef's directions for creating a knife.rb." if chef_config[:cookbook_path].blank?
+    cl_path = chef_config[:cookbook_path].map{|dir| File.expand_path('../clusters', dir) }.uniq
+    ui.warn "No cluster path set. Taking a wild guess that #{cl_path.inspect} is \nreasonable based on your cookbook_path -- but please set cluster_path in your knife.rb"
+    chef_config[:cluster_path] = cl_path
   end
 
-  def ui=(ui) @ui = ui ; end
-  def ui()    @ui      ; end
+  def realms_dir
+    clusters_dir.map{ |dir| File.expand_path('../realms', dir) }.uniq
+  end
 
-  def chef_config=(cc) @chef_config = cc ; end
-  def chef_config()    @chef_config      ; end
+  def load_cluster name
+    raise ArgumentError.new('Please supply a cluster name') if name.blank?
+    load_dsl_definition(name, clusters_dir)
+    clusters[name.to_sym] or die("Couldn't find a cluster definition for <#{name}> in #{clusters_dir}")
+  end
 
-  # execute against multiple targets in parallel
-  def parallel(targets)
-    raise 'missing block' unless block_given?
+  def load_realm name
+    raise ArgumentError.new('Please supply a realm name') if name.blank?
+    load_dsl_definition(name, realms_dir)
+    realms[name.to_sym] or die("Couldn't find a realm definition for <#{name}> in #{realms_dir}")
+  end
+
+  def load_dsl_definition(name, location)
+    Chef::Log.info "Looking for <#{name}> dsl definition in #{location}"
+    if named_file = dsl_files(location).detect{ |dsl_file| File.basename(dsl_file, '.rb') == name }
+      load_dsl_file named_file
+    else
+      dsl_files(location).each{ |f| load_dsl_file f }
+    end
+  end
+
+  def dsl_files location
+    Dir.glob File.join(location, '**/*.rb')    
+  end
+
+  def load_dsl_file filename
+    Chef::Log.info "Loading dsl file #{filename}"
+    require filename
+  end
+
+  #
+  # Common utility methods
+  #
+  def die(*messages)
+    exit_code = messages.last.is_a?(Integer) ? messages.pop : -1
+    messages.each{ |msg| ui.warn msg }
+    exit exit_code
+  end
+
+  def parallel(targets, &actions)
+    raise ArgumentError.new('Must provide a block to run in parallel') unless block_given?
     results = []
     [targets].flatten.each_with_index.map do |target, idx|
       sleep(0.25) # avoid hammering with simultaneous requests
@@ -48,169 +118,36 @@ module Ironfan
     results
   end
 
-  #
-  # Defines a cluster with the given name.
-  #
-  # @example
-  #   Ironfan.cluster 'demosimple' do
-  #     cloud :ec2 do
-  #       availability_zones  ['us-east-1d']
-  #       flavor              "t1.micro"
-  #       image_name          "ubuntu-natty"
-  #     end
-  #     role                  :base_role
-  #     role                  :chef_client
-  #
-  #     facet :sandbox do
-  #       instances 2
-  #       role                :nfs_client
-  #     end
-  #   end
-  #
-  #
-  def cluster(name, attrs={}, &block)
-    name = name.to_sym
-    # If this is being called as Ironfan.cluster('foo') with no additional arguments,
-    # return the cached cluster object if it exists
-    if @@clusters[name] and attrs.empty? and not block_given?
-      return @@clusters[name]
-    else # Otherwise we're being asked to (re)initialize and cache a cluster definition
-      cl = Ironfan::Dsl::Cluster.new(:name => name)
-      cl.receive!(attrs, &block)
-      @@clusters[name] = cl.resolve
-    end
-  end
-
-  def realm(name, attrs={}, &block)
-    name = name.to_sym
-    if @@realms[name] and attrs.empty? and not block_given?
-      return @@realms[name]
-    else
-      rlm = Ironfan::Dsl::Realm.new(:name => name)
-      rlm.receive!(attrs, &block)
-      rlm.clusters.keys.each{|k| @@clusters[k.to_sym] = rlm.clusters[k].resolve}
-      @@realms[name] = rlm
-    end
-  end
-
-  def load_realm(name)
-    name = name.to_sym
-    raise ArgumentError, "Please supply a realm name" if name.to_s.empty?
-    return @@realms[name] if @@realms[name]
-
-    load_cluster_files
-
-    unless @@realms[name] then die("Couldn't find a realm definition for #{name} in #{cluster_path}") end
-
-    @@realms[name]
-  end
-
-  #
-  # Return cluster if it's defined. Otherwise, search Ironfan.cluster_path
-  # for an eponymous file, load it, and return the cluster it defines.
-  #
-  # Raises an error if a matching file isn't found, or if loading that file
-  # doesn't define the requested cluster.
-  #
-  # @return [Ironfan::Cluster] the requested cluster
-  def load_cluster(name)
-    name = name.to_sym
-    raise ArgumentError, "Please supply a cluster name" if name.to_s.empty?
-    return @@clusters[name] if @@clusters[name]
-
-    load_cluster_files
-
-    unless @@clusters[name] then die("Couldn't find a cluster definition for #{name} in #{cluster_path}") end
-
-    @@clusters[name]
-  end
-
-  def load_cluster_files
-    cluster_path.each do |cp_dir|
-      Dir[ File.join(cp_dir, '*.rb') ].each do |filename|
-        Chef::Log.info("Loading cluster file #{filename}")
-        require filename
-        clusters.values.each{|cluster| cluster.source_file ||= filename}
-      end
-    end
-  end
-
-  #
-  # Map from cluster name to file name
-  #
-  # @return [Hash] map from cluster name to file name
-  def cluster_filenames
-    return @cluster_filenames if @cluster_filenames
-    @cluster_filenames = {}
-    cluster_path.each do |cp_dir|
-      Dir[ File.join(cp_dir, '*.rb') ].each do |filename|
-        cluster_name = File.basename(filename).gsub(/\.rb$/, '')
-        @cluster_filenames[cluster_name.to_sym] ||= filename
-      end
-    end
-    @cluster_filenames
-  end
-
-  #
-  # Utility to die with an error message.
-  # If the last arg is an integer, use it as the exit code.
-  #
-  def die *strings
-    exit_code = strings.last.is_a?(Integer) ? strings.pop : -1
-    strings.each{|str| ui.warn str }
-    exit exit_code
-  end
-
-  #
-  # Utility to turn an error into a warning
-  #
-  # @example
-  #   Ironfan.safely do
-  #     Ironfan.fog_connection.associate_address(self.fog_server.id, address)
-  #   end
-  #
-  def safely(info="")
+  def safely(operation = '', &action)
     begin
       yield
     rescue StandardError => err
-      ui.warn("Error running #{info}:")
-      ui.warn(err)
-      Chef::Log.error( err )
-      Chef::Log.error( err.backtrace.join("\n") )
+      ui.warn "Error running #{operation}:"
+      ui.warn err
+      Chef::Log.error err
+      Chef::Log.error err.backtrace.join("\n")
       return err
     end
   end
 
-  #
-  # Utility to retry a flaky operation three times, with ascending wait times
-  #
-  # FIXME: Add specs to test the rescue here. It's a PITA to debug naturally or
-  #
-  # Manual test:
-  # bundle exec ruby -e "require 'chef'; require 'ironfan'; Ironfan.tell_you_thrice { p 'hah'; raise 'hell' }"
-  def tell_you_thrice(options={})
-    options = { name:           "problem",
-                error_class:    StandardError,
-                retries:        3,
-                multiplier:     3 }.merge!(options)
-    try     = 0
-    message = ''
+  def tell_you_thrice(options = {}, &action)
+    error_class = options[:error_class] || StandardError
+    retries     = options[:retries]     || 3
+    multiplier  = options[:multiplier]  || 3  
 
+    attempt = 0
     begin
-      try += 1
+      attempt += 1
       yield
-    rescue options[:error_class] => err
-      raise unless try < options[:retries]
-      pause_for = options[:multiplier] * try
+    rescue error_class => err
+      raise if attempt > retries
+      pause_for = multiplier * attempt
       Chef::Log.debug "Caught error (was #{err.inspect}). Sleeping #{pause_for} seconds."
       sleep pause_for
       retry
     end
   end
 
-  #
-  # Utility to show a step of the overall process
-  #
   def step(name, desc, *style)
     ui.info("  #{"%-15s" % (name.to_s+":")}\t#{ui.color(desc.to_s, *style)}")
   end
@@ -220,18 +157,14 @@ module Ironfan
   end
 
   def verbosity
-    chef_config[:verbosity].to_i
+    knife_config[:verbosity].to_i
   end
 
-  # Output a TODO to the logs if you've switched on pestering
   def todo(*args)
-    Chef::Log.debug(*args) if Chef::Config[:show_todo]
+    Chef::Log.debug(*args) if knife_config[:show_todo]
   end
 
-  #
-  # Utility to do mock out a step during a dry-run
-  #
-  def unless_dry_run
+  def unless_dry_run(&action)
     if dry_run?
       ui.info("      ... but not really")
       return nil
@@ -239,12 +172,12 @@ module Ironfan
       yield
     end
   end
+
   def dry_run?
-    chef_config[:dry_run]
+    knife_config[:dry_run]
   end
 
-  # Intentionally skipping an implied step
-  def noop(source,method,*params)
+  def noop(source, method, *params)
     # Chef::Log.debug("#{method} is a no-op for #{source} -- skipping (#{params.join(',')})")
   end
 end
