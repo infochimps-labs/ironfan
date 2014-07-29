0c72356346b92fdda58f7ef6b90f484f1d70332f
Josh Bronson <josh.bronson@gmail.com>
2014-07-29 12:50:25 -0500

    feature: support provisioned IOPS
    Change-Id: I4c67b093172bdbe1b854eebaa6e7e113c0b2ef15

--- lib/ironfan/provider/ec2/machine.rb
+++ lib/ironfan/provider/ec2/machine.rb
@@ -271,6 +271,8 @@ module Ironfan
           end
           if cloud.flavor_info[:ebs_optimizable]
             description[:ebs_optimized] = cloud.ebs_optimized
+          elsif cloud.ebs_optimized
+            Chef::Application.fatal!("ebs_optimized set but #{cloud.flavor} does not support ebs optimization!")
           end
           description
         end
@@ -291,6 +293,8 @@ module Ironfan
               end
               hsh['Ebs.SnapshotId'] = volume.snapshot_id if volume.snapshot_id.present?
               hsh['Ebs.VolumeSize'] = volume.size.to_s   if volume.size.present?
+              hsh['Ebs.VolumeType'] = volume.type        if volume.type.present?
+              hsh['Ebs.Iops']       = volume.iops        if volume.type == 'io1'
               hsh['Ebs.DeleteOnTermination'] = (not volume.keep).to_s
             else next
             end
